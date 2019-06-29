4c63a3d7ed709a613dee0cc40654b0e35eb335ba
Walter Quispe <walter.quissan@gmail.com>
2019-06-29 00:52:15 -0500

    Modificacion Compras

--- Presentacion/FormTodoEdicion/frmCompras.Designer.vb
+++ Presentacion/FormTodoEdicion/frmCompras.Designer.vb
@@ -1,17 +1,14 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmCompra
-    Inherits System.Windows.Forms.Form
+Partial Class frmCompras
+    Inherits Presentacion.frmTodoEdicion
 
-    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    'Form invalida a Dispose para limpiar la lista de componentes.
     <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        Try
-            If disposing AndAlso components IsNot Nothing Then
-                components.Dispose()
-            End If
-        Finally
-            MyBase.Dispose(disposing)
-        End Try
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
     End Sub
 
     'Requerido por el Diseñador de Windows Forms
@@ -23,7 +20,6 @@ Partial Class frmCompra
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "frmCompra"
     End Sub
+
 End Class
